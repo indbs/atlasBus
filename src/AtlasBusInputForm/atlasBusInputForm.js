d31db9c7073ba43c2843b2f5e6b2291f7b586d93
Boris <b.smirnov@rusgates.ru>
2019-09-18 18:59:50 +0300

    Пустой шаблон

---
+++ src/AtlasBusInputForm/atlasBusInputForm.js
@@ -0,0 +1,14 @@
+import   React                              from 'react';
+import { Formik }                           from 'formik';
+
+export class AtlasBusInputForm extends React.Component{
+  render(){
+    return(
+      <div>
+        test me
+      </div>
+    )
+  }
+}
+
+export default AtlasBusInputForm;
