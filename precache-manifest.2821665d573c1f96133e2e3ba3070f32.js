b8b6f6bd94232b8cf83df0f5fd8a841c81312372
smirnov.boris <b.smirnov@rusgates.ru>
2019-09-23 10:51:37 +0300

    Updates

---
+++ precache-manifest.2821665d573c1f96133e2e3ba3070f32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f441802e77136efbc1063684712e9c",
+    "url": "/atlasBus/index.html"
+  },
+  {
+    "revision": "aeb6394c0cffd14cbb00",
+    "url": "/atlasBus/static/css/2.e734d5b2.chunk.css"
+  },
+  {
+    "revision": "1f33f6fc8d675d0b5ca6",
+    "url": "/atlasBus/static/css/main.e0968aaf.chunk.css"
+  },
+  {
+    "revision": "aeb6394c0cffd14cbb00",
+    "url": "/atlasBus/static/js/2.bf8193e0.chunk.js"
+  },
+  {
+    "revision": "1f33f6fc8d675d0b5ca6",
+    "url": "/atlasBus/static/js/main.acb94e5b.chunk.js"
+  },
+  {
+    "revision": "eb6d68e7ba19ce34bd47",
+    "url": "/atlasBus/static/js/runtime~main.dc544b00.js"
+  }
+]);
